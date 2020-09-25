ea06c35f0ca95d23651523915951f2ee55c1dc13
Steven Garcia <sjosegarcia23@gmail.com>
2020-09-25 11:58:58 -0400

    Guarding App if no token is available

--- lib/authentication/authentication_guard.dart
+++ lib/authentication/authentication_guard.dart
@@ -2,8 +2,9 @@ import 'package:atletico_app/data/token.dart';
 import 'package:auto_route/auto_route.dart';
 
 class AuthenticationGuard extends RouteGuard {
- @override
- Future<bool> canNavigate(ExtendedNavigatorState navigator, String routeName, Object arguments) async {
-   return Token.loadToken() == Token();
- }
+  @override
+  Future<bool> canNavigate(ExtendedNavigatorState navigator, String routeName,
+      Object arguments) async {
+    return Token.loadToken() == Token();
+  }
 }
