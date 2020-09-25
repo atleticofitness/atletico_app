6c5e1ef405e39a7e8954d8dec487f9e455b7dd49
Steven Garcia <sjosegarcia23@gmail.com>
2020-09-25 12:00:24 -0400

    Added the guard to the routers

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -6,7 +6,10 @@ import 'package:auto_route/auto_route_annotations.dart';
 
 @MaterialAutoRouter(pathPrefix: "/app/v1", routes: <AutoRoute>[
   MaterialRoute(path: "/", page: LoginWidget, initial: true),
-  MaterialRoute(path: "/registration", page: RegistrationWidget, guards: [AuthenticationGuard]),
+  MaterialRoute(
+      path: "/registration",
+      page: RegistrationWidget,
+      guards: [AuthenticationGuard]),
   //MaterialRoute(path: "/confirm_email", page: RegistrationWidget),
   MaterialRoute(path: "/atletico", page: AtleticoWidget)
 ])
